09c41104a5c09a7c7621d73d9c19871b94c6a80f
jeffgarrett <jeff2invest@gmail.com>
2021-06-29 08:03:34 -0700

    some skaffolding for registries

--- docmir/src/models/Registries.ts
+++ docmir/src/models/Registries.ts
@@ -1,5 +1,25 @@
 
-
 export class Registries {
-  
+
+//****** Private members *******//
+  private _imageRepositories: string [];
+
+//***** Constructure and Methods *****//
+  constructor() {
+  }
+
+  public printState(){}
+
+
+//***** Getter and Setters *******//
+  get imageRepositories(): string[] {
+    return this._imageRepositories;
+  }
+
+  set imageRepositories(value: string[]) {
+    this._imageRepositories = value;
+  }
+
+
+
 }

1e64c78ddb10611bb0193ef5ad07720584675d15
jeffgarrett <jeff2invest@gmail.com>
2021-07-04 13:24:20 -0700

    new files

---
+++ docmir/test/commands/searchtags.test.ts
@@ -0,0 +1,17 @@
+import {expect, test} from '@oclif/test'
+
+describe('searchtags', () => {
+  test
+  .stdout()
+  .command(['searchtags'])
+  .it('runs hello', ctx => {
+    expect(ctx.stdout).to.contain('hello world')
+  })
+
+  test
+  .stdout()
+  .command(['searchtags', '--name', 'jeff'])
+  .it('runs hello --name jeff', ctx => {
+    expect(ctx.stdout).to.contain('hello jeff')
+  })
+})

9aa7724e81390174df39e3877c412bac20c23872
jeffgarrett <jeff2invest@gmail.com>
2021-07-25 12:47:16 -0700

    adding new files

---
+++ docmir/src/commands/sync.ts
@@ -0,0 +1,28 @@
+import {Command, flags} from '@oclif/command'
+import {SyncController} from '../controllers/SyncController'
+
+export default class Sync extends Command {
+  static description = 'Syncs all tags missing in target registry. \n' +
+    'Note: you must create a sync first before syncing the entire repo at this point.  This is best used for when you want to keep a sync up to date'
+
+  static flags = {
+    help: flags.help({char: 'h'}),
+
+  }
+
+  static args = [
+    {name: 'repo', require: true, },
+    {name: 'provider'}
+  ]
+
+  async run() {
+    const {args, flags} = this.parse(Sync)
+    const syncController = new SyncController()
+
+    syncController.diffTags(args.repo, args.provider)
+
+
+
+
+  }
+}
